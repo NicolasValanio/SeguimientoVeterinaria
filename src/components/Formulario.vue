ad4ba061e84943a77d0cf1cf0ba3d0df53816716
Nicolas Valenzuela <serviciosvalenzuela1@gmail.com>
2023-10-26 04:36:45 -0500

    priemra sesion formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+</script>
+
+
+<template >
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade Pacientes y
+            <span class="text-indigo-600 font-bold">Administralos</span>
+        </p>
+
+        <form class="bg-white shadow-2xl rounded-lg py-10 px-5 mb-10">
+            <div class="mb-5">
+                <label for="mascota" class="text-gray-700 uppercase font-bold">Nombre Mascota</label> 
+
+                <input type="text" id="mascota" placeholder="Nombre de la mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+
+            </div>
+
+            <div class="mb-5">
+                <label for="propietario" class="text-gray-700 uppercase font-bold">Nombre Propietario</label> 
+
+                <input type="text" id="propietario" placeholder="Nombre del propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+
+            </div>
+
+            
+            <div class="mb-5">
+                <label for="email" class="text-gray-700 uppercase font-bold">Email</label> 
+
+                <input type="email" id="email" placeholder="Email del propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+
+            </div>
+
+            
+            <div class="mb-5">
+                <label for="alta" class="text-gray-700 uppercase font-bold">Alta</label> 
+
+                <input type="date" id="date" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+
+            </div>
+
+            
+            <div class="mb-5">
+                <label for="sintomas" class="text-gray-700 uppercase font-bold">Sintomas</label> 
+
+                <textarea name="sintomas" id="sintomas" placeholder="Describe los síntomas" class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"></textarea>
+            </div>
+
+            <input type="submit" value="Registrar paciente" class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors">
+        </form>
+    </div>
+</template>
